fa9ba6e56298dc4c06d77f49f48707220af2f72d
David Johnson <dcharles525@gmail.com>
2014-09-11 22:15:50 -0400

    added mod 1 and 2 of what ive done

---
+++ Module 01/Mod01 Assignments/StudentInfoCard/StudentInfoCard.java
@@ -0,0 +1,29 @@
+/**
+ * displaying my student information
+ * 
+ * @author David Johnson
+ * @version 9/10/14
+ */
+public class StudentInfoCard
+{
+    public static void main(String[] args)
+    {
+        System.out.println("****************************************************************************");
+        System.out.println("*                                                                          *");
+        System.out.println("*                          Student Information                             *");
+        System.out.println("*                                                                          *");
+        System.out.println("****************************************************************************");
+        System.out.println("                                                                            ");
+        System.out.println("  Name: David Johnson           Birthday: 5/25/1997             Age: 17     ");
+        System.out.println("                                                                            ");
+        System.out.println("  School: Fort Mill High School       Grade: 12           City: Fort Mill   ");
+        System.out.println("                                                                            ");
+        System.out.println("  Home and Cell Phone: 1(803)-554-1186   Available: 4:00pm - 1:00am Mon-Fri ");
+        System.out.println("                                                                            ");
+        System.out.println("  Current Math: Pre-Calc CP                                                 ");
+        System.out.println("  Programming Experience: HTML, CSS, PHP, C, C#, VB, and Linux Terminal     ");
+        System.out.println("                                                                            ");
+        System.out.println("  I'm taking this course because I love computer programming!!! <3          ");
+       
+    }
+}
