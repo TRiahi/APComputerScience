cb492e3071b4432ce41eb7fa8231ed3dba5dcf30
David Johnson <dcharles525@gmail.com>
2014-09-22 20:10:18 -0400

    working on finishing mod2

--- Module 02/Mod02 Assignments/2.05 Units of Measurement/2.05 Units of Measurement/MeasurementConverterV1.java
+++ Module 02/Mod02 Assignments/2.05 Units of Measurement/2.05 Units of Measurement/MeasurementConverterV1.java
@@ -11,8 +11,8 @@
  * @author B. Jordan
  * @version 01/19/07
  *
- * Modified by: (Student name goes here)
- * Date: (Today's date goes here)
+ * Modified by: David Johnson
+ * Date: 9/18/14
  */
 public class MeasurementConverterV1
 {
@@ -24,21 +24,28 @@ public class MeasurementConverterV1
         double kilometers;              // distance in kilometers 
         double pounds;                  // weight in pounds
         double kilograms;               // weight in kilograms
+        double liters;                  // amount in liters
+        double gallons;                 // amount in gallons
+        double tbsp;                    // amount in tablespoon
+        double tsp;                     // amount in teaspoon
         double feetPerMile = 5280;      // number of feet per 1 mile
         double kilometerPerMile = 1.60934; // number of kilometers in one mile 
         double kilogramPerPound = 0.453592; //number of kilograms per pound
+        double literPerGallon = 3.78541;//number of liters per gallon
+        double tablespoonPerTeaspoon = 3; //number of tablespoon per teaspoon
 
         //convert feet to miles
         feet = 6230;
-        miles = feet / feetPerMile;
+        miles = feet / feetPerMile; 
         System.out.println( feet + " ft. = " + miles + " mi.");
-
-
+        
         //convert miles to feet
         miles = 3;
         feet = miles * feetPerMile;
         System.out.println( miles + " mi. = " + feet + " ft. " );
-
+        
+        System.out.println();
+        
         //miles to lkilometers 
         miles = 6;
         kilometers = miles * kilometerPerMile;
@@ -49,6 +56,8 @@ public class MeasurementConverterV1
         miles = kilometers / kilometerPerMile;
         System.out.println( miles + " mi. = " + kilometers + " km. " );
               
+        System.out.println();
+        
         //pounds to kilograms 
         pounds = 30;
         kilograms = pounds * kilogramPerPound;
@@ -59,6 +68,29 @@ public class MeasurementConverterV1
         pounds = kilograms / kilogramPerPound;
         System.out.println( pounds + " lb. = " + kilograms + " kg. " );
         
+        System.out.println();
+        
+        //gallons to liters
+        gallons = 4;
+        liters = gallons * literPerGallon;
+        System.out.println( gallons + " gal. = " + liters + "lit." );
+        
+        //liters to gallons
+        liters = 65;
+        gallons = liters / literPerGallon;
+        System.out.println( liters + " lit. = " + gallons + " gal." );
+        
+        System.out.println();
+        
+        //tablespoons to teaspooon
+        tbsp = 45;
+        tsp = tbsp * tablespoonPerTeaspoon;
+        System.out.println( tbsp + " tbsp. = " + tsp + " tsp." );
+        
+        //teaspoon to tablespoon
+        tsp = 57;
+        tbsp = tsp / tablespoonPerTeaspoon;
+        System.out.println( tsp + " tsp. = " + tbsp + " tbsp. " );
         
     }//end of main method
 }//end of class
