12455fd1756fed5d8c71ca2516610ab4af1a726c
David Johnson <dcharles525@gmail.com>
2014-09-23 20:36:47 -0400

    up to 3.04

--- Module 02/Mod02 Assignments/Assignment 02.08 Challenge Program/Assignment 02.08 Challenge Program/CurrencyConversionV1.java
+++ Module 02/Mod02 Assignments/Assignment 02.08 Challenge Program/Assignment 02.08 Challenge Program/CurrencyConversionV1.java
@@ -7,6 +7,10 @@
  * �CSA/FLVS 2011
  * @author Maria Vieta Jacquard
  * @version 09/01/11
+ * 
+ * Modified By: David Johnson
+ * Modified On: 9/23/14
+ * 
  */
 public class CurrencyConversionV1
 {
@@ -21,7 +25,7 @@ public class CurrencyConversionV1
         double yenExchangeRate = 0.00972;       // local variable for exchange rate of Yen
         double dollarsSpentInMexico = 0.0;      // local variable for dollars spent in Mexico
         double dollarsSpentInJapan = 0.0;       // local variable for dollars spent in Japan
-        double dollarsSpentInFrance = 0.0       // local 
+        double dollarsSpentInFrance = 0.0;      // local 
         double remainingUsDollars = 0.0;        // local variable for US Dollars remaining
         //remaining variables below here
 
@@ -38,22 +42,24 @@ public class CurrencyConversionV1
         
         // convertion for Mexican pesos
         dollarsSpentInMexico = pesosSpent * pesoExchangeRate;
-        System.out.println("US dollars spent in Mexico:    " + dollarsSpentInMexico);
         remainingUsDollars = startingUsDollars - dollarsSpentInMexico;
         
         // convertion for Japanese yen
         dollarsSpentInJapan = yenSpent * yenExchangeRate;
-        System.out.println("US dollars spent in Japan:     " + dollarsSpentInJapan);
         remainingUsDollars = remainingUsDollars - dollarsSpentInJapan;
 
         // convertion for Euros
         dollarsSpentInFrance = euroSpent * euroExchangeRate;
-        remainingUsDollars = remainingUsDollars -  dollarsSpentInEuro;
-
+        remainingUsDollars = remainingUsDollars -  dollarsSpentInFrance;
 
         //print output to the screen
-        // code goes below here
-
+        System.out.println("US dollars spent in Mexico:    " + dollarsSpentInMexico);
+        System.out.println("US dollars spent in Japan:     " + dollarsSpentInJapan);
+        System.out.println("US dollars spent in France     " + dollarsSpentInFrance);
+        System.out.println("======================================================");
+        System.out.println("Remaining US dollars:          " + remainingUsDollars);
+        System.out.println();
+        System.out.println();
 
         // Complete the code below. Replace th 0's for totalItem and fundsRemaining
         // with the proper calculations. Casting, integer division and the modulus
@@ -67,9 +73,12 @@ public class CurrencyConversionV1
         //Calculations for Souvenir #1
         int costItem1 = 4;                          //cost per item of first souvenir
         int budget1 = 50;                           //budget for first item
-        int totalItem1 = 0;         //how many items can be purchased
+        int totalItem1 = 12;         //how many items can be purchased
         double fundsRemaining1 = 0.0;  //how much of the budget is left
-
+        
+        fundsRemaining1 = budget1 / totalItem1;
+        fundsRemaining1 = (int)budget1 % costItem1;
+        
         System.out.println("Item 1");
         System.out.println("   Cost per item: $" + costItem1 );
         System.out.println("   Budget: $"+ budget1);
@@ -79,9 +88,12 @@ public class CurrencyConversionV1
         //Calculations for Souvenir #2
         double costItem2 = 32.55;                       //cost per item of second souvenir
         int budget2 = 713;                              //budget for second item
-        int totalItem2 = 0;     //how many items can be purchased
+        int totalItem2 = 21;     //how many items can be purchased
         double fundsRemaining2 = 0.0;   //how much of the budget is left
-
+        
+        fundsRemaining2 = budget2 / totalItem2;
+        fundsRemaining2 = (int)budget2 % costItem2;
+        
         System.out.println("Item 2");
         System.out.println("   Cost per item: $" + costItem2 );
         System.out.println("   Budget: $"+ budget2);
---
+++ Module 03/Mod3 Assignments/3.04 ASCII Art/ArtWork.java
@@ -0,0 +1,92 @@
+
+/**
+ * ASCII Art, I made a person!
+ * 
+ * @author David Johnson
+ * @version 9/23/14
+ */
+public class ArtWork
+{
+    public static void main(String [ ] args)
+    {
+        //local variables
+        String row1 = "                         *******                                       \n";
+        String row2 = "                       *         *                                     \n";
+        String row3 = "                     *    0    0  *                                    \n";
+        String row4 = "                     *             *                                   \n";
+        String row5 = "                     *      /      *                                   \n"; 
+        String row6 = "                     *             *                                   \n";
+        String row7 = "                      *   ******  *                                    \n";
+        String row8 = "                       *         *                                     \n";
+        String row9 = "                        *********                                      \n";
+        String row10 = "             ***********         ***********                           \n";
+        String row11 = "             *      *               *      *                           \n";
+        String row12 = "             *      *               *      *                           \n";
+        String row13 = "             *      *               *      *                           \n";
+        String row14 = "             *      *               *      *                           \n";
+        String row15 = "             *      *               *      *                           \n";
+        String row16 = "             *      *               *      *                           \n";
+        String row17 = "             *      *               *      *                           \n";
+        String row18 = "             *      *               *      *                           \n";
+        String row19 = "             *      *               *      *                           \n";
+        String row20 = "             *      *               *      *                           \n";
+        String row21 = "             *      *               *      *                           \n";
+        String row22 = "             * **** *///////////////* **** *                           \n";
+        String row23 = "                    *      / /      *                                  \n";
+        String row24 = "                    *      / /      *                                  \n";
+        String row25 = "                    *      / /      *                                  \n";
+        String row26 = "                    *      / /      *                                  \n";
+        String row27 = "                    *      / /      *                                  \n";
+        String row28 = "                    *      / /      *                                  \n";
+        String row29 = "                    *      / /      *                                  \n";
+        String row30 = "                    *      / /      *                                  \n";
+        String row31 = "                    *      / /      *                                  \n";
+        String row32 = "                    *      / /      *                                  \n";
+        String row33 = "                    *      / /      *                                  \n";
+        String row34 = "                    *      / /      *                                  \n";
+        String row35 = "                    *      / /      *                                  \n";
+        String row36 = "                    *      / /      *                                  \n";
+        String row37 = "                    *      / /      *                                  \n";
+        String row38 = "            *********      / /      *                                  \n";
+        String row39 = "            *************************                                  \n";
+        
+        System.out.print(row1);
+        System.out.print(row2);
+        System.out.print(row3);
+        System.out.print(row4);
+        System.out.print(row5);
+        System.out.print(row6);
+        System.out.print(row7);
+        System.out.print(row8);
+        System.out.print(row9);
+        System.out.print(row10);
+        System.out.print(row11);
+        System.out.print(row12);
+        System.out.print(row13);
+        System.out.print(row14);
+        System.out.print(row15);
+        System.out.print(row16);
+        System.out.print(row17);
+        System.out.print(row18);
+        System.out.print(row19);
+        System.out.print(row20);
+        System.out.print(row21);
+        System.out.print(row22);
+        System.out.print(row23);
+        System.out.print(row24);
+        System.out.print(row25);
+        System.out.print(row26);
+        System.out.print(row27);
+        System.out.print(row28);
+        System.out.print(row29);
+        System.out.print(row30);
+        System.out.print(row31);
+        System.out.print(row33);
+        System.out.print(row34);
+        System.out.print(row35);
+        System.out.print(row36);
+        System.out.print(row37);
+        System.out.print(row38);
+        System.out.print(row39);
+    }
+} 
