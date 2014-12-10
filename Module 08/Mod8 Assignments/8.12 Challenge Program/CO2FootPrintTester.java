77d8e1856b0349bc36f8cf75570950bf0acd42a5
David <dcharles525@gmail.com>
2014-12-10 12:02:44 -0500

    Mod 8 done, starting mod 9

--- Module 08/Mod8 Assignments/8.12 Challenge Program/CO2FootPrintTester.java
+++ Module 08/Mod8 Assignments/8.12 Challenge Program/CO2FootPrintTester.java
@@ -13,6 +13,13 @@ public class CO2FootPrintTester
         double [] gas = {48000,38400};
         double [] electricity = {16440,16111};
         double [] waste = {1018,3054};
+        double [] recycling = {2045,3654};
+        double [] bulbs = {5,9};
+        
+        CO2Footprint vars = new CO2Footprint(gas,electricity,waste,recycling,bulbs);
+        
+        double [] reduction = vars.emissionReductionBulbs();
+        double [] reductionTotal = vars.emissionReduction();
         
         System.out.println("|              Pounds of CO2              |       Pounds of CO2       |                      |");
         System.out.println("|              Emmitted from              |        Reduce from        |                      |");
@@ -21,7 +28,7 @@ public class CO2FootPrintTester
         
         for (int i = 0; i < 2; i++){
             
-            System.out.printf("%s%10.2f%3s%12.2f%4s\n","|",gas[i],"|",electricity[i],"|");
+            System.out.printf("%s%10.2f%3s%12.2f%4s%9.2f%4s%11.2f%4s%9.2f%4s%16.2f%7s\n","|",gas[i],"|",electricity[i],"|",waste[i],"|",recycling[i],"|",reduction[i],"|",reductionTotal[i],"|");
         
         }
         
--- Module 08/Mod8 Assignments/8.12 Challenge Program/CO2Footprint.java
+++ Module 08/Mod8 Assignments/8.12 Challenge Program/CO2Footprint.java
@@ -1,9 +1,43 @@
 public class CO2Footprint
 {
-
-    CO2Footprint(){
+    
+    double [] gas, electricity, waste, recycling, bulbs;
+    
+    CO2Footprint(double [] g, double [] e, double [] w, double [] r, double [] b){
         
+        gas = g;
+        electricity = e;
+        waste = w;
+        recycling = r;
+        bulbs = b;
         
+    }
+    
+    public double [] emissionReductionBulbs(){
+       
+       double [] total = {0,0};
+       
+       for (int i = 0; i < total.length; i++){
+           
+           total[i] = bulbs[i] * 1.37 * 73;
+        
+       }
+        
+       return total; 
+        
+    }
+    
+    public double [] emissionReduction(){
+        
+        double [] total = {0,0};
+        
+        for (int i = 0; i < gas.length; i++){
+            
+            total[i] = (gas[i] + electricity[i] + waste[i]) - (recycling[i] + bulbs[i]);
+            
+        }
+        
+        return total;
         
     }
     
