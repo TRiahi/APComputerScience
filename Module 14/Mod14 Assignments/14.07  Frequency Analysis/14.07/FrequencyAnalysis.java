f44138b8bc10c16e1cafa8513c53717e50754c42
David Johnson <dcharles525@gmail.com>
2015-02-10 12:57:10 -0500

    Finished 14.07

--- Module 14/Mod14 Assignments/14.07  Frequency Analysis/14.07/FrequencyAnalysis.java
+++ Module 14/Mod14 Assignments/14.07  Frequency Analysis/14.07/FrequencyAnalysis.java
@@ -9,17 +9,19 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter; 
 
 public class FrequencyAnalysis{
      
     public static void main(String[] args)throws IOException{
         
-        int A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z, i = 0;
+        int i = 0, f = 0;
         String [] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","N","M","O"
         ,"P","Q","R","S","T","U","V","W","X","Y","Z"};
-        String message = "";
+        int [] lettersCount = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        String message = "", letter="";
         
-        File messageS = new File(".txt");
+        File messageS = new File("plaintext.txt");
         Scanner in = new Scanner(System.in);
         Scanner messageSS = new Scanner(messageS);
         
@@ -32,13 +34,13 @@ public class FrequencyAnalysis{
         
         while (i < message.length()){
             
-            String letter = message.substr(i, i + 1);
+            letter = message.substring(i, i + 1);
             
             while (f < 26){
                 
-                if (letter.equals(letters[f])){
-                
+                if (letter.toUpperCase().equals(letters[f])){
                    
+                   lettersCount[f] += 1;
                 
                 }
                 
@@ -46,11 +48,27 @@ public class FrequencyAnalysis{
                 
             }
             
+            f = 0;
             i++;
             
         }
         
-        System.out.println(message);
+        for (int g = 0; g < 26; g++){
+            
+            System.out.println(letters[g] + ": " + lettersCount[g]);
+            
+        }
+        
+        //printing to file
+        PrintWriter outFile = new PrintWriter (new File("plaintextfreq.txt")); 
+        
+        for (i = 0; i < 26; i++){
+            
+            outFile.println(lettersCount[i]);
+            
+        }
+        
+        outFile.close ();
          
     }
     
