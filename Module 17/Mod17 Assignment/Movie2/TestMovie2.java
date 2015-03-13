63c507bb771e7d5eb48da279fcda8f07a481f4e4
David Johnson <dcharles525@gmail.com>
2015-03-13 13:06:05 -0400

    starting mod 17

--- Module 17/Mod17 Assignment/Movie2/TestMovie2.java
+++ Module 17/Mod17 Assignment/Movie2/TestMovie2.java
@@ -4,6 +4,10 @@
  * @author David Johnson
  * @version 3/12/15
  */
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class TestMovie2{
     
     public static void main(String[] args){
@@ -15,44 +19,53 @@ public class TestMovie2{
        myMovies[0][1] = "2001";
        myMovies[0][2] = "Columbia Tristar";
        
-	   myMovies[1][0] = "Mulan Special Edition"; 
-	   myMovies[1][1] = "2004"; 
-	   myMovies[1][2] = "Disney";   
-	   
-	   myMovies[2][0] = "Shrek 2"; 
-	   myMovies[2][1] = "2004"; 
-	   myMovies[2][2] = "Dreamworks";   
-	   
+       myMovies[1][0] = "Mulan Special Edition"; 
+       myMovies[1][1] = "2004"; 
+       myMovies[1][2] = "Disney";   
+       
+       myMovies[2][0] = "Shrek 2"; 
+       myMovies[2][1] = "2004"; 
+       myMovies[2][2] = "Dreamworks";   
+       
        myMovies[3][0] = "The Incredibles"; 
-	   myMovies[3][1] = "2004"; 
-	   myMovies[3][2] = "Pixar";
-	   
-	   myMovies[4][0] = "Nanny MacPhee"; 
-	   myMovies[4][1] = "2006"; 
-	   myMovies[4][2] = "Universal";
-	   
-	   myMovies[5][0] = "The Curse of the Were-Rabbit"; 
-	   myMovies[5][1] = "2006"; 
-	   myMovies[5][2] = "Aardman";
-	   
-	   myMovies[6][0] = "Ice Age"; 
-	   myMovies[6][1] = "2002"; 
-	   myMovies[6][2] = "20th Century Fox";
-	   
-	   myMovies[7][0] = "Lilo & Stitch"; 
-	   myMovies[7][1] = "2002"; 
-	   myMovies[7][2] = "Disney";
-	   
-	   myMovies[8][0] = "Robots"; 
-	   myMovies[8][1] = "2005"; 
-	   myMovies[8][2] = "20th Century Fox";
-	   
-	   myMovies[9][0] = "Monsters Inc."; 
-	   myMovies[9][1] = "2001"; 
-	   myMovies[9][2] = "Pixar";
-	   
-	   printMovies(myMovies, arraySize);
-	   
+       myMovies[3][1] = "2004"; 
+       myMovies[3][2] = "Pixar";
+       
+       myMovies[4][0] = "Nanny MacPhee"; 
+       myMovies[4][1] = "2006"; 
+       myMovies[4][2] = "Universal";
+       
+       myMovies[5][0] = "The Curse of the Were-Rabbit"; 
+       myMovies[5][1] = "2006"; 
+       myMovies[5][2] = "Aardman";
+       
+       myMovies[6][0] = "Ice Age"; 
+       myMovies[6][1] = "2002"; 
+       myMovies[6][2] = "20th Century Fox";
+       
+       myMovies[7][0] = "Lilo & Stitch"; 
+       myMovies[7][1] = "2002"; 
+       myMovies[7][2] = "Disney";
+       
+       myMovies[8][0] = "Robots"; 
+       myMovies[8][1] = "2005"; 
+       myMovies[8][2] = "20th Century Fox";
+       
+       myMovies[9][0] = "Monsters Inc."; 
+       myMovies[9][1] = "2001"; 
+       myMovies[9][2] = "Pixar";
+       
+       //printMovies(myMovies, arraySize);
+       //String myMoviesSorted[] = sortTitles(myMovies,arraySize, 0);
+       //String myMoviesSorted[] = sortYears(myMovies,arraySize, 1);
+       String myMoviesSorted[] = sortStudio(myMovies,arraySize, 1);
+
+       for (int i = 0; i < arraySize; i++){
+       
+           System.out.println(myMoviesSorted[i]);
+           
+       }
+       
     }
     
     public static void printMovies(String[][] myMovies, int arraySize){
@@ -70,6 +83,78 @@ public class TestMovie2{
             System.out.println();
             
         }
+        
+    }
+    
+    public static String[] sortTitles(String[][] myMovies, int arraySize, int option){
+        
+        String[] myMoviesSorted = new String[arraySize];
+        
+        for (int i = 0; i < arraySize; i++){
+           
+            myMoviesSorted[i] = myMovies[i][0];
+                    
+        }
+        
+        if (option == 1){
+              
+            Arrays.sort(myMoviesSorted); 
+            
+        }else{
+            
+            Arrays.sort(myMoviesSorted,  Collections.reverseOrder()); 
+           
+        }
+        
+        return myMoviesSorted;
+        
+    }
+    
+    public static String[] sortYears(String[][] myMovies, int arraySize, int option){
+        
+        String[] myMoviesSorted = new String[arraySize];
+        
+        for (int i = 0; i < arraySize; i++){
+           
+            myMoviesSorted[i] = myMovies[i][1];
+                    
+        }
+        
+        if (option == 1){
+              
+            Arrays.sort(myMoviesSorted); 
+            
+        }else{
+            
+            Arrays.sort(myMoviesSorted,  Collections.reverseOrder()); 
+           
+        }
+        
+        return myMoviesSorted;
+        
+    }
+    
+    public static String[] sortStudio(String[][] myMovies, int arraySize, int option){
+        
+        String[] myMoviesSorted = new String[arraySize];
+        
+        for (int i = 0; i < arraySize; i++){
+           
+            myMoviesSorted[i] = myMovies[i][2];
+                    
+        }
+        
+        if (option == 1){
+              
+            Arrays.sort(myMoviesSorted); 
+            
+        }else{
+            
+            Arrays.sort(myMoviesSorted,  Collections.reverseOrder()); 
+           
+        }
+        
+        return myMoviesSorted;
         
     }
     
